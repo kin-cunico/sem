1996c751d6346103ba8dff819ab1919602e5d343
Kin Cunico <contact@kincunico.dev>
2025-09-18 21:33:48 +0100

    Refactored App class to move methods outside of psvm, added getters, and methods to make it interactive

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,5 @@
 package com.napier.sem;
-
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -8,15 +7,53 @@ public class App {
         InputReq inputReq = new InputReq();
     }
     public static class InputReq {
-        private String name;
         Scanner input = new Scanner(System.in);
+        private String name;
+        private String age;
         public InputReq() {
-            getName();
+            promptName();
+            greetUser();
+            guessAge();
+
         }
-        public void getName() {
+        private void promptName() {
             System.out.println("Please enter your name: ");
-            name = input.nextLine();
-            System.out.println("Hello, " + name);
+            this.name = input.nextLine();
+        }
+
+        private void promptAge() {
+            System.out.println("What is your age?");
+            this.age = input.nextLine();
+        }
+
+        private void greetUser() {
+            System.out.printf("Hello, %s!",
+                    getName()
+            );
+        }
+
+        private void guessAge() {
+            Random rand = new Random();
+            String guessedAge = Integer.toString(rand.nextInt(1, 99));
+
+            System.out.printf("\n I think I can guess your age. You are %s! \n", guessedAge);
+            promptAge();
+
+            if (getAge().equals(guessedAge)) {
+                System.out.println(" \n I guessed it right! :)");
+
+            }
+            else {
+                System.out.println(" \n Oh, I thought I had it right :(");
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAge() {
+            return age;
         }
     }
 }
