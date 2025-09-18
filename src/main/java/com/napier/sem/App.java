b933c21f3b7e5f6acedc5685698b3f22675e6a65
Kin Cunico <contact@kincunico.dev>
2025-09-18 21:10:21 +0100

    Refactored App class to move methods outside of psvm

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String name;
-
-        System.out.println("Please enter your name: ");
+        InputReq inputReq = new InputReq();
+    }
+    public static class InputReq {
+        private String name;
         Scanner input = new Scanner(System.in);
-        name = input.nextLine();
-
-        System.out.println("Hello, " + name);
+        public InputReq() {
+            getName();
+        }
+        public void getName() {
+            System.out.println("Please enter your name: ");
+            name = input.nextLine();
+            System.out.println("Hello, " + name);
+        }
     }
 }
