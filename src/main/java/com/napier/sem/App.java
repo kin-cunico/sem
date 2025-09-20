9f66788b543da4aed13f81ce2e2c62024631b14e
Kin Cunico <contact@kincunico.dev>
2025-09-20 16:56:27 +0100

    created a mongodb request class to handle requests to database;

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,12 +1,17 @@
 package com.napier.sem;
 import com.napier.sem.utils.InputReq;
+import com.napier.sem.utils.MongReq;
 
 
 public class App {
     public static void main(String[] args) {
-        InputReq inputReq = new InputReq();
+/*        InputReq inputReq = new InputReq();
         inputReq.promptName();
         inputReq.greetUser();
-        inputReq.guessAge();
+        inputReq.guessAge();*/
+
+        MongReq mongReq = new MongReq();
+
+        System.out.println(mongReq.getDoc().toJson());
     }
 }
